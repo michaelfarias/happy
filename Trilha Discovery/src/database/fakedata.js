60bc2dba2d9003c166a3011bbcb3e21f1819cc57
michaelfarias <michaelfarias01@gmail.com>
2020-12-03 15:52:03 -0300

    refactor: integrando o NODE

---
+++ Trilha Discovery/src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633",
+        lng: "-49.6455874",
+        name: "Lar das meninas",
+        description: "Presta assistencia a crianças de 06 15 anos que  se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1595207011175-3d72f5a3b21c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+            "https://images.unsplash.com/photo-1595207011175-3d72f5a3b21c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+            "https://images.unsplash.com/photo-1595207011175-3d72f5a3b21c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        ],
+        instructions: "Venha se sentir a vontade e traga muito amos e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-27.222633",
+        lng: "-49.5455874",
+        name: "Lar dos meninos",
+        description: "Presta assistencia a crianças de 06 15 anos que  se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+            "https://images.unsplash.com/photo-1595207011175-3d72f5a3b21c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+            "https://images.unsplash.com/photo-1595207011175-3d72f5a3b21c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
+            "https://images.unsplash.com/photo-1595207011175-3d72f5a3b21c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxfDB8MXxyYW5kb218fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080"
+        ],
+        instructions: "Venha se sentir a vontade e traga muito amos e paciência para dar.",
+        opening_hours: "Horário de visitas Das 18h até 8h",
+        open_on_weekends: "0"
+    }
+]
